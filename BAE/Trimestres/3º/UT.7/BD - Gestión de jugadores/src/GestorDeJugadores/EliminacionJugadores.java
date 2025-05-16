0940b77065e023706479a16411e1bb91ef9dee7a
Rodrigo <135568662+Rodrigo-LPz@users.noreply.github.com>
2025-05-16 03:51:56 +0100

    Add files via upload

---
+++ BAE/Trimestres/3º/UT.7/BD - Gestión de jugadores/src/GestorDeJugadores/EliminacionJugadores.java
@@ -0,0 +1,180 @@
+/**
+ *
+ * @author Rodrigo
+ */
+package GestorDeJugadores;
+
+// Importaciones de librerías de la base de datos de MongoDB.
+    // Importa todos los paquetes de la API "client", como por ejemplo "com.mongodb.client.MongoClient" o "com.mongodb.client.MongoClients".
+    import com.mongodb.client.*;
+    // Importa el paquete "Document".
+    import org.bson.Document;
+
+// Importaciones de librerías de Java.
+    // Importa todos los paquetes de la API "util", como por ejemplo "java.util.Scanner".
+    import java.util.*;
+
+
+// Se crea la clase "EliminacionJugadores" esta hará la función de gestionar la eliminación de jugadores registrados en nuestra colección.
+public class EliminacionJugadores{
+    // Declara un método privado para realizar operaciones CRUD dentro de una colección de documentos en MongoDB.
+    private final MongoCollection<Document> collection;
+
+    // Crea un constructor que recibe la colección, el atributo privado "collection" creado anteriormente.
+    public EliminacionJugadores(MongoCollection<Document> collection){
+        this.collection = collection;
+    }
+
+    // Declara un método público, "ejecutarEliminacionJugador", para ejecutar la acción de eliminar a un jugador de la colección.
+    public void ejecutarEliminacionJugador(){
+        // Crea un escáner con el que poder interactuar con el usuario a través de la consola.
+        Scanner user = new Scanner(System.in);
+
+        // Muestreo de instrucciones previas.
+        System.out.println("\nA continuación siga las indicaciones dadas introduciendo los valores acorde a lo que se le solicite en el momento:");
+
+        // Muestreo de la acción a realizar.
+        System.out.println("\n\n\t<=== ELIMINACIÓN DE JUGADOR ====>");
+
+        // Crea unas funciones con las que filtrar, seleccionar y realizar la operación sobre los datos desde la base de datos de "MongoDB".
+        Document filtroJugador, jugadorFiltrado, jugadores;
+
+        // Asignación de variables numéricas y no numéricas.
+        String campo, campoFinal, parametro, respuesta, respuestaConfirmada;
+        int numeroDeLista;
+
+        // Pregunta al usuario por el campo a buscar a dicho jugador en concreto/específico.
+        System.out.println("\n¿Por cuál campo/parámetro le gustaría empezar a filtrar la búsqueda del jugador? (Nombre / Dorsal / Posición)"); /* Se obvia el parámetro "Edad" pues para la acción que se va a ejecutar posteriormente no es coherente buscar a dicho jugador por su edad, esto se debe a que por lo general, puede darse el caso de que al menos dos jugadores compartan la misma edad. */
+        campo = user.nextLine().trim();
+        campoFinal = Character.toUpperCase(campo.charAt(0)) + campo.substring(1).toLowerCase();
+
+        // Crea un bucle de tipo "while" a modo de comprobante para validar el valor final obtenido o introducido en la variable "campoFinal".
+        while (!campoFinal.equals("Nombre") && !campoFinal.equals("Dorsal") && !campoFinal.equals("Posición") && !campoFinal.equals("Posicion")){
+            System.out.println("ERROR: El valor introducido como campo de búsqueda o filtrado {" + campoFinal + "} no es válido. Recuerde que el valor del campo a introducir deberá ser 'Nombre', 'Dorsal' o 'Posición'.");
+            
+            // Brinda una nueva oportunidad de introducir el valor correcto.
+            campo = user.nextLine().trim();
+            campoFinal = Character.toUpperCase(campo.charAt(0)) + campo.substring(1).toLowerCase();
+        }
+
+        // Muestreo del siguiente paso/acción a realizar por el usuario.
+        System.out.println("\nEn ese caso, por favor, introduzca el valor de parámetro de búsqueda del campo {" + campoFinal + "} a buscar: ");
+        parametro = user.nextLine().trim();
+
+        // Crea un condicional de tipo "if" para realizar la búsqueda de jugadores por dorsal.
+        if (campoFinal.equals("Dorsal")){
+            // Crea un bloque de tipo "try-catch" para efectuar la búsqueda del filtro sobre el/los jugador/es que entren dentro del parámetro introducido.
+            try{
+                int parametroNumerico = Integer.parseInt(parametro);
+                filtroJugador = new Document(campoFinal, parametroNumerico);
+            } catch (NumberFormatException  e){
+                System.err.println("ERROR: El valor introducido como parámetro de búsqueda {" + parametro + "} para el campo {" + campoFinal + "} no es válido. Recuerde que el valor introducido como parámetro deberá ser un número entero válido.");
+                return;
+            }
+
+        // Crea el caso contrario del anterior condicional, del "if", para realizar la búsqueda de jugadores por nombre o posición.
+        } else{
+            /*
+              *
+               *
+                * La expresión regular "$regex" indica que se usará una expresión regular (búsqueda por patrón/token), esto a modo de dirección.
+                * Todo esto en conjunto dará como resultado que el valor de la variable "parámetro" a buscar dentro del "campo" tendrá que ser estrictamente igual al valor introducido por el usuario, sin que haya ningún otro carácter antes o después de la cadena de texto.
+                * Por otro lado, la expresión regular "$options" se utiliza, es es una clave de "MongoDB" que permite modificar el comportamiento del "regex".
+                * En cambio, la expresión regular "i" (de "insensitive"), indica que no debe importar si es mayúscula o minúscula.
+               *
+              *
+            */
+            filtroJugador = new Document(campoFinal, new Document("$regex", parametro).append("$options", "i")); /* Se realiza una búsqueda de tipo "Flexible" a fines de que se encuentre un jugador en una cobertura de caso mayor, más amplia. */
+        }
+
+        // Crea una lista donde se almacenarán temporalmente los jugadores que coincidan con el filtro de búsqueda aplicado.
+        List<Document> coincidencias = new ArrayList<>();
+
+        // Crea un bucle de tipo "for-each" con el que recorrer cada documento, jugador, que coincida con el filtro aplicado y añadirlo a la lista "coincidencias".
+        for (Document jugador : collection.find(filtroJugador)){
+            coincidencias.add(jugador); /* Lo que se está haciendo es crear una colección de datos temporal, en este caso una lista, "List"; donde almacenar los jugadores coincidentes al filtro creado anteriormente. Y, finalmente, con el bucle, añadir ese jugador a la lista. */
+        }
+
+        // Declara un condicional de tipo "if" con el que recoger el/los casos en el que el valor de la variable "coincidencias" se encuentre vacío.
+        if (coincidencias.isEmpty()){
+            System.err.println("ERROR: Dado el filtro de búsqueda impuesto {" + filtroJugador + "} no se ha encontrado ningún jugador que coincida con la búsqueda dentro o propio a la colección que se está trabajando.");
+            return;
+
+        // Crea un condicional de tipo "if" para acoger el/los casos en el que el filtro obtenga y arroje como resultado a más de un jugador de forma simultánea.
+        } else if (coincidencias.size() > 1){
+            // Muestreo del caso por pantalla.
+            System.out.println("\nDado el filtro de búsqueda impuesto, esta ha resultado en varios jugadores:");
+
+            // Crea un bucle de tipo "for" con el que recorrer cada jugador almacenado (encontrado) en la lista temporal, en "coincidencias", e irlos mostrando por pantalla.
+            for (int i = 0; i < coincidencias.size(); i++){
+                jugadores = coincidencias.get(i);
+                System.out.println("\n" + (i + 1) + ".\t Nombre: " + jugadores.getString("Nombre") + " | Edad: " + jugadores.getInteger("Edad") + " | Dorsal: " + jugadores.getInteger("Dorsal") + " | Posición: " + jugadores.getString("Posición"));
+            }
+
+            // Declara un bucle de tipo "while" con el que seleccionar al jugador concreto a modificar/actualizar (sus registros/datos).
+            while (true){
+                // Muestreo de las instrucciones a seguir:
+                System.out.print("\nSeleccione el número de lista del jugador que desea eliminar: ");
+
+                // Declara un condicional de tipo "if" a fin de comprobar que el valor introducido por el usuario es numérico, es decir, un número entero válido.
+                if (user.hasNextInt()){
+                    numeroDeLista = user.nextInt();
+                    user.nextLine(); /* Limpia el buffer, de esta manera nos aseguramos de que la lectura de código durante la ejecución no se salte nada. */
+
+                    // Crea un condicional de tipo "if" para poder comprobar que el número introducido esté comprendido dentro del rango de opciones disponibles.
+                    if (numeroDeLista >= 1 && numeroDeLista <= coincidencias.size()){
+                        break; /* Valida el valor introducido y sale del condicional, del "if". */
+                    } else{
+                        System.err.println("ERROR: El valor/número de lista introducido {" + numeroDeLista + "} no es reconocido por nuestro sistema o se encuentra fuera del rango de opciones disponibles. Inténtelo nuevamente.");
+                    }
+
+                // Crea una cobertura para los casos en los que el usuario introduzca un valor no numérico.
+                } else{
+                    System.err.println("ERROR: El sistema reconoce que el valor que está introduciendo no corresponde con un valor numérico válido. Inténtelo de nuevo.");
+                    user.nextLine(); /* Limpia el buffer, de esta manera nos aseguramos de que la lectura de código durante la ejecución no se salte nada. */
+                }
+            }
+
+            // Asigna a la variable "jugadorFiltrado" el jugador correspondiente a la opción elegida.
+            jugadorFiltrado = coincidencias.get(numeroDeLista - 1); /* Se empieza restando por (1) ya que las posiciones de los índices comienzan en (0). */
+
+        // Cubre el caso restante, aquel en el que sólo haya un jugador en la lista de coincidencias, asignándole directamente su correspondiente a "jugadorFiltrado".
+        }else{
+            jugadorFiltrado = coincidencias.get(0); /* Dado que es el único jugador que ha pasado los filtros, que tiene dichas coincidencias, ocupará la primera posición, que en los índices será el (0). */
+        }
+
+        // Muestreo de los datos obtenidos, de los jugadores encontrados.
+        System.out.println("\nEl jugador seleccionado a eliminar es:\n\tNombre: " + jugadorFiltrado.getString("Nombre") + "\n\tEdad: " + jugadorFiltrado.getInteger("Edad") + " años\n\tDorsal: " + jugadorFiltrado.getInteger("Dorsal") + "\n\tPosición: " + jugadorFiltrado.getString("Posición"));
+
+        // Confirmación de la acción 'borrado' al usuario para mayor seguridad.
+        System.out.print("\n{" + jugadorFiltrado + "} es el jugador seleccionado. ¿Está seguro de querer eliminarlo? → (Sí / No): ");
+        respuesta = user.nextLine().trim();
+        respuestaConfirmada = Character.toUpperCase(respuesta.charAt(0)) + respuesta.substring(1).toLowerCase();
+
+        // Declara un condicional de tipo "if" con el que cubrir los casos de respuestas 'No' a la confirmación de borrado.
+        if (respuestaConfirmada.equals("No")){
+            // Muestreo del resultado.
+            System.out.println("Acción de borrado declinada por el usuario. El jugador no ha sido eliminado/borrado de la colección.");
+            return;
+
+        // Cobertura de tipo condicional "else-if" para los casos de respuesta "No".
+        } else if (!respuestaConfirmada.equals("Sí") && !respuestaConfirmada.equals("Si")){
+            System.err.println("ERROR: El valor introducido como respuesta {" + respuestaConfirmada + "} no es válido. Recuerde que la respuesta a introducir deberá ser o bien 'Sí' o bien 'No'.");
+            return;
+        }
+
+        // Ejecuta la acción de borrado/eliminación usando el ID del documento.
+        ObjectId idJugador = jugadorFiltrado.getObjectId("_id"); /* El atributo "_id" se asigna automáticamente a cada documento que se crea bajo "MongoDB". Este actuará como identificador único de dicho documento (Como si fuese un DNI). */
+        DeleteResult resultado = collection.deleteOne(new Document("_id", idJugador)); /* Recoge o coteja el objeto/documento, jugador, que comparta la misma dirección, el mismo 'ID' y después lo elimina de la colección. */
+
+        // Muestreo del resultado final de la ejecución.
+        if (resultado.getDeletedCount() > 0){
+            System.out.println("\nEl jugador seleccionado {" + jugadorFiltrado + "} ha sido eliminado/borrado correctamente de la colección.");
+        } else {
+            System.err.println("ERROR: Fallo en la eliminación del jugador. Intente nuevamente.");
+        }
+
+        // Cierra el escáner.
+        user.close();
+    }
+}
---
+++ BAE/Trimestres/3º/UT.7/BD - Gestión de jugadores/src/GestorDeJugadores/ListadoJugadores.java
@@ -0,0 +1,188 @@
+/**
+ *
+ * @author Rodrigo
+ */
+package GestorDeJugadores;
+
+// Importaciones de librerías de la base de datos de MongoDB.
+    // Importa todos los paquetes de la API "client", como por ejemplo "com.mongodb.client.MongoClient" o "com.mongodb.client.MongoClients".
+    import com.mongodb.client.*;
+    // Importa el paquete "Document".
+    import org.bson.Document;
+
+// Importaciones de librerías de Java.
+    // Importa todos los paquetes de la API "util", como por ejemplo "java.util.Scanner".
+    import java.util.*;
+
+
+// Se crea la clase "ListadoJugadores" esta hará la función de listar/buscar el registro de los jugadores que se encuentren dentro de la colección.
+public class ListadoJugadores{
+    // Declara un método privado para realizar operaciones CRUD dentro de una colección de documentos en MongoDB.
+    private final MongoCollection<Document> collection;
+
+    // Crea un constructor que recibe la colección, el atributo privado "collection" creado anteriormente.
+    public ListadoJugadores(MongoCollection<Document> collection){
+        this.collection = collection;
+    }
+
+    // Declara un método público, "ejecutarListadoJugador", para ejecutar la acción de listar/buscar a un jugador.
+    public void ejecutarListadoJugador(){
+        // Crea un escáner con el que poder interactuar con el usuario a través de la consola.
+        Scanner user = new Scanner(System.in);
+        
+        // Muestreo de instrucciones previas.
+        System.out.println("\nA continuación siga las indicaciones dadas introduciendo los valores acorde a lo que se le solicite en el momento:");
+
+        // Muestreo de la acción a realizar.
+        System.out.println("\n\n\t<=== LISTADO DE JUGADORES REGISTRADOS ====>\n");
+
+        // Recupera todos los documentos, jugadores, de la colección.
+        FindIterable<Document> jugadores = collection.find();
+
+        // Crea una función con la que filtrar datos desde la base de datos de "MongoDB".
+        Document filtroJugador;
+
+        // Creación de las variables a usar.
+        String nombre, posicion;
+        Integer edad, dorsal;
+        
+        // Declara una variable de tipo "boolean" con el fin de controlar si la colección está vacía.
+        boolean existenJugadores = false;
+
+        // Crea un bucle de tipo "for-each" con el que recorrer y mostrar todos los documentos, jugadores, de la colección.
+        for (Document jugador : jugadores){
+            existenJugadores = true;
+
+            nombre = jugador.getString("Nombre");
+            edad = jugador.getInteger("Edad");
+            dorsal = jugador.getInteger("Dorsal");
+            posicion = jugador.getString("Posición");
+
+            System.out.println("Nombre    : " + nombre);
+            System.out.println("Edad      : " + edad + " años");
+            System.out.println("Dorsal    : " + dorsal);
+            System.out.println("Posición  : " + posicion);
+            System.out.println("<-------------------------------------------------------------------------->\n\n");
+        }
+
+        // Crea un condicional de tipo "if" para comprobar si la colección está vacía o no.
+        if (!existenJugadores){
+            System.err.println("ERROR: La colección sobre la que se está trabajando no existe o no contiene ningún registro, jugadores, (Está vacía).");
+        }
+
+        // Creación de las variables a usar.
+        String respuesta, respuestaFinal, campo, campoFinal, parametro, parametroEstricto, parametroEstrictoFinal;
+        
+        // Pregunta al usuario si desea buscar a un jugador en concreto/específico.
+        System.out.print("\n¿Desea buscar un jugador específico dentro de la colección de jugadores? → (Sí / No): ");
+        respuesta = user.nextLine().trim();
+        respuestaFinal = Character.toUpperCase(respuesta.charAt(0)) + respuesta.substring(1).toLowerCase();
+
+        if (!respuestaFinal.equals("No")){
+            return; /* Se termina la búsqueda de jugadores en caso de que la respuesta no haya sido "Sí" o "Si" */
+        } else if (!respuestaFinal.equals("Sí") && !respuestaFinal.equals("Si")){ /* No volvemos a filtrar en este "if" el posible "!respuestaFinal.equals("No")" pues ya está filtrado o declarado en el anterior "if". */
+            System.out.println("ERROR: El valor de la respuesta introducida {" + respuestaFinal + "} no es válido. Recuerde que la respuesta a introducir deberá ser 'Sí' o 'No'.");
+            return;
+        } else{
+            // Muestreo de los posibles opciones a escoger para filtrar la búsqueda del jugador.
+            while (true){
+                System.out.println("\n¿Y por cuál campo/parámetro le gustaría empezar a filtrar la búsqueda del jugador? (Nombre / Edad/ / Dorsal / Posición)");
+                campo = user.nextLine().trim();
+                campoFinal = Character.toUpperCase(campo.charAt(0)) + campo.substring(1).toLowerCase();
+
+                // Crea un bucle de tipo "if" con el que poder salir de bucle "while" en caso de que el usuario haya introducido correctamente el campo con el que trabajar.
+                if (campoFinal.equals("Nombre") || campoFinal.equals("Edad") || campoFinal.equals("Dorsal") || campoFinal.equals("Posición") || campoFinal.equals("Posicion")){
+                    break; /* Valida el valor introducido y sale del condicional, del "if". */
+                } else{
+                    System.out.println("ERROR: El valor introducido como campo de búsqueda o filtrado {" + campoFinal + "} no es válido. Recuerde que el valor del campo a introducir deberá ser 'Nombre', 'Edad', 'Dorsal' o 'Posición'.");
+                }
+            }
+
+            // Petición del nuevo dato/parámetro para filtrar la búsqueda del jugador.
+            System.out.print("\nEn ese caso, por favor, introduzca el nuevo valor de parámetro de búsqueda del campo {" + campoFinal + "} a buscar: ");
+            parametro = user.nextLine().trim();
+            
+            // Crea un condicional de tipo "if" para realizar la búsqueda de jugadores por edad o dorsal.
+            if (campoFinal.equals("Edad") || campoFinal.equals("Dorsal")){
+                // Crea un bloque de tipo "try-catch" para efectuar la búsqueda del filtro sobre el/los jugador/es que entren dentro del párametro introducido.
+                try{
+                    int parametroNumerico = Integer.parseInt(parametro);
+                    filtroJugador = new Document(campoFinal, parametroNumerico);
+                } catch (NumberFormatException  e){
+                    System.err.println("ERROR: El valor introducido como párametro de búsqueda {" + parametro + "} para el campo {" + campoFinal + "} no es válido. Recuerde que el valor introducido como parámetro deberá ser un número entero válido.");
+                    return;
+                }
+
+            // Crea el caso contrario del anterior condicional, del "if", para realizar la búsquedaBúsqueda de jugadores por nombre o posición.
+            } else{
+                // Por defecto, asignaremos a la variable "parametroEstrictoFinal" el valor "Flexible" a fines de que la búsqueda pueda llegar a darse, teniendo esta un valor por defecto.
+                parametroEstrictoFinal = "Flexible";
+                
+                // Crea un bucle de tipo "while" con el que comprobar que el valor introducido para el tipo de búsqueda concuerde.
+                while (true){
+                    // Muestreo de las opciones de filtrado que hay para la búsqueda en caso de haber escogido como "campo" a "Nombre" o "Posición".
+                    System.out.println("\nDado que has escogido el campo de búsqueda {" + campoFinal + "} ¿Prefiere una búsqueda directa o flexible? (Directa / Flexible)\n(una búsqueda directa es aquella que busca el jugador exacto dado un parámetro, por ejemplo: si buscas por el parámetro de nombre 'Rodri', queriendo encontrar al jugador 'Rodrigo', no lo encontrarás, pues estás buscando a un jugador con el registro de 'Rodri'. En cambio, una búsqueda flexible es aquella que busca al/a los jugador/es que empiecen por 'Rodri', pudiendo obtener más jugadores tales como 'Rodri', 'Rodrigo', 'Rodriguez', etc.).");
+
+                    parametroEstricto = user.nextLine().trim();
+                    parametroEstrictoFinal = Character.toUpperCase(parametroEstricto.charAt(0)) + parametroEstricto.substring(1).toLowerCase();
+                    
+                    // Declara un condicional de tipo "if" para verificar si el valor introducido para la variable "parametroEstricto" es uno de los dos (en este caso) esperados.
+                    if (parametroEstrictoFinal.equals("Directa") && parametroEstrictoFinal.equals("Flexible")){
+                        break; /* Se comprueba que el valor es el esperado, es correcto y se sale del condicional "if". */
+                    } else{
+                        System.err.println("Error: El valor introducido como párametro para el tipo de búsqueda no es válido. Recuerde que el valor a introducir deberá ser sólo 'Directa' o 'Flexible': ");
+                    }
+                }
+
+                // Crea un condicional de tipo "if" para el caso de que la variable "parametroEstrictoFinal" reciba como valor "Directa".
+                if (parametroEstrictoFinal.equals("Directa")){
+                    /*
+                      *
+                       *
+                        * La expresión regular "$regex" indica que se usará una expresión regular (búsqueda por patrón/token), esto a modo de dirección.
+                        * La expresión regular "^" significa que la búsqueda tendrá que: “empezar por/con…”.
+                        * La expresión regular "$" significa que la búsqueda tendrá que: terminar por/con…”.
+                        * Todo esto en conjunto dará como resultado que el valor de la variable "parámetro" a buscar dentro del "campo" tendrá queser estrictamente igual al valor introducido por el usuario, sin que haya ningún otro carácter antes o después de la cadena de texto.
+                        * Por otro lado, la expresión regular "$options" se utiliza, es es una clave de "MongoDB" que permite modificar el comportamiento del "regex".
+                        * En cambio, la expresión regular "i" (de "insensitive"), indica que no debe importar si es mayúscula o minúscula.
+                       *
+                      *
+                    */
+                    filtroJugador = new Document(campoFinal, new Document("$regex", "^" + parametro + "$").append("$options", "i"));
+                
+                // Crea un condicional de tipo "if" para cubrir el otro caso, donde la variable "parametroEstrictoFinal" reciba como valor "Flexible".
+                } else if (parametroEstrictoFinal.equals("Flexible")){
+                    filtroJugador = new Document(campoFinal, new Document("$regex", parametro).append("$options", "i"));
+                }
+
+            }
+            // Activa el filtro y ejecuta la búsqueda del/de los jugador/es que coincidan con el parámetro introducido dentro del campo solicitado.
+            FindIterable<Document> resultado = collection.find(filtroJugador);
+
+            // Declara una variable de tipo "boolean" con un valor inicial "false" con el fin de manejar si la colección está vacía respecto al/a los jugador/es que se estén buscando.
+            boolean resultadoJugadorEncontrado = false;
+            
+            // Muestreo del resultado final de la búesqueda filtrada.
+            System.out.println("\n\n\t<=== RESULTADO DE BÚSQUEDA ===>\n");
+
+            // Repite el proceso con un nuevo bucle de tipo "for-each".
+            for (Document jugador : resultado){
+                resultadoJugadorEncontrado = true;
+
+                System.out.println("Nombre    : " + jugador.getString("Nombre"));
+                System.out.println("Edad      : " + jugador.getInteger("Edad") + " años");
+                System.out.println("Dorsal    : " + jugador.getInteger("Dorsal"));
+                System.out.println("Posición  : " + jugador.getString("Posición"));
+                System.out.println("<-------------------------------------------------------------------------->\n\n");
+            }
+
+            // Crea un condicional de tipo "if" para comprobar si la búsqueda da un resultado o no en base a los documentos, jugadores, registrados en la colección.
+            if (!resultadoJugadorEncontrado){
+                System.err.println("ERROR: Fallo en nuestro sistema al encontrar el/los jugador/es coincidentes con los datos ingresados como parámetro de búsqueda {" + parametro +"} para/dentro del campo " + campoFinal + ". O bien ese jugador no existe.");
+            }
+        }
+
+        // Cierra el escáner.
+        user.close();
+    }
+}
